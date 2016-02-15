63e6762288f392f683ecd46f0141da814c5b9c19
Gabriel Nakano <gtoshinakano@gmail.com>
2016-02-15 10:29:20 -0200

    Sending mail, but not flexible

---
+++ mail-options.js
@@ -0,0 +1,31 @@
+﻿var fs = require('fs');
+var attachPath = __dirname + "/contas/anexos";
+var filesArray = fs.readdirSync(attachPath);
+
+// Formatted Attachment array for Nodemailer
+var makeAttachmentArray = function(files){
+	
+	var ret = [];
+	for(var i=0; i < files.length; i++){
+		var pushItem = {
+			fileName: files[i],
+			filePath: attachPath + "/" + files[i]
+		}
+		ret.push(pushItem);
+	}
+	return ret;
+	
+}
+
+// Returned on sendmail.js with email props
+var mailOptions = {
+    from: 'DAEE DSD/ADA <avelez@sp.gov.br>', // sender address
+	replyTo : "avelez@sp.gov.br",
+    to: 'gtoshinakano@gmail.com', // list of receivers
+    subject: 'Teste Mailer', // Subject line
+	text: 'Em observância ao Item I do Artigo 4º da Portaria DAEE-389, de 03/02/2016, anexamos ao presente correio eletrônico...', // plaintext body
+    attachments: makeAttachmentArray(filesArray)
+	//html: '<b>Hello world ??</b>' // html body
+};
+
+module.exports = mailOptions;
---
+++ sendmail.js
@@ -0,0 +1,23 @@
+﻿var nodemailer = require('nodemailer');
+
+var mailOpt = require('./mail-options');
+
+// create reusable transporter object using the default SMTP transport
+var smtp_options = {
+	service: "Gmail",
+    auth: {
+		user: "daee.contas@gmail.com",
+        pass: "QaWsEd123"
+	}
+}
+var transporter = nodemailer.createTransport("SMTP", smtp_options);
+
+console.log(mailOpt);
+
+// send mail with defined transport object
+transporter.sendMail(mailOpt, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response );
+});
