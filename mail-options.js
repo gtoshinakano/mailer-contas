2e90e4a3e5a11d4ff11db456f96329c94e5dc8cf
Gabriel Nakano <gtoshinakano@gmail.com>
2016-02-15 16:43:55 -0200

    Sending email with dinamic text/html.

--- mail-options.js
+++ mail-options.js
@@ -1,31 +1,66 @@
 ﻿var fs = require('fs');
 var attachPath = __dirname + "/contas/anexos";
 var filesArray = fs.readdirSync(attachPath);
+var dirData = JSON.parse(fs.readFileSync('diretorias.json', 'utf8'));
+var meses = ["Mes","Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
 
-// Formatted Attachment array for Nodemailer
+
+/******************************************************************
+ * CAMPOS PARA SEREM ALTERADOS A CADA ENVIO DE CONTAS
+ * PARA CONSULTAR DIRETORIAS, VÁ EM diretorias.json
+ * PARA ALTERAR NÚMEROS PROCURE PELO VENCIMENTO EM diretorias.json
+ ******************************************************************/
+var diretoria = dirData.BBT;
+var telefones = diretoria.telefones.venc24;
+var mesRef = 2;
+var anoRef = 2016;
+var vencimento = "24/02/2016";
+var responderPara = "avelez@sp.gov.br";
+
+
+// Attachment array for Nodemailer, from /contas/anexos
 var makeAttachmentArray = function(files){
 	
-	var ret = [];
+	var _ref = (mesRef < 10) ? '0' + mesRef : mesRef; // mês de referência para nome de arquivo
+	var _ret = [];
 	for(var i=0; i < files.length; i++){
 		var pushItem = {
-			fileName: files[i],
+			fileName: diretoria.sigla + '_' + anoRef + '-' + _ref + '_vencto_' + vencimento.replace(/\//g, '-') + '_' + i + '-' + meses[mesRef] +'.pdf', // nome do arquivo
 			filePath: attachPath + "/" + files[i]
 		}
-		ret.push(pushItem);
+		_ret.push(pushItem);
 	}
-	return ret;
+	return _ret;
 	
 }
 
+// Check if all info and attachments are ok.
+var isValidToSendEmail = function(files, telefones){
+	
+	console.log('Qtd Anexos: ' + files.length);
+	console.log('Qtd Telefones: ' + telefones.length);
+	return (files.length > 0 && telefones.length > 0);
+	
+}
+
+var mailBodyHtml = '<b>Senhor Diretor da '+ diretoria.sigla +',</b><br /><p>Em observância ao Item I do Artigo 4º da Portaria DAEE-389, de 03/02/2016, anexamos ao presente correio eletrônico a(s) conta(s) digitalizada(s) da(s) linha(s): ';
+mailBodyHtml += telefones.toString() + 'referente(s) ao mês de <b>'+ meses[mesRef] + ' de ' + anoRef +'</b>, instalada(s) em unidade(s) dessa Diretoria, com vencimento em '+ vencimento +', para que seja(m) ratificada(s) por Vossa Senhoria.</p>';
+mailBodyHtml += '<p>A ratificação poderá ser feita  no próprio corpo deste correio eletrônico e nos enviada como resposta.</p><br /><br />';
+
+var mailSubject = 'Atestado de contas telefônicas ref. '+ meses[mesRef] + ' de ' + anoRef +' - '+ diretoria.sigla;
+
 // Returned on sendmail.js with email props
 var mailOptions = {
-    from: 'DAEE DSD/ADA <avelez@sp.gov.br>', // sender address
-	replyTo : "avelez@sp.gov.br",
-    to: 'gtoshinakano@gmail.com', // list of receivers
-    subject: 'Teste Mailer', // Subject line
-	text: 'Em observância ao Item I do Artigo 4º da Portaria DAEE-389, de 03/02/2016, anexamos ao presente correio eletrônico...', // plaintext body
-    attachments: makeAttachmentArray(filesArray)
-	//html: '<b>Hello world ??</b>' // html body
+
+    from : 'DAEE DSD/ADA <avelez@sp.gov.br>', // sender address
+	replyTo : responderPara,
+    to: diretoria.email,
+    subject: mailSubject, // Subject line
+	generateTextFromHTML: true,
+	html: mailBodyHtml, // html body
+    attachments: makeAttachmentArray(filesArray),
+	isComplete : isValidToSendEmail(filesArray, telefones) // to send or not
+
 };
 
 module.exports = mailOptions;
--- sendmail.js
+++ sendmail.js
@@ -1,5 +1,6 @@
-﻿var nodemailer = require('nodemailer');
+﻿console.log('Verificando arquivos para envio de email.');
 
+var nodemailer = require('nodemailer');
 var mailOpt = require('./mail-options');
 
 // create reusable transporter object using the default SMTP transport
@@ -12,12 +13,19 @@ var smtp_options = {
 }
 var transporter = nodemailer.createTransport("SMTP", smtp_options);
 
-console.log(mailOpt);
+// console.log(mailOpt);
+
 
 // send mail with defined transport object
-transporter.sendMail(mailOpt, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response );
-});
+if(mailOpt.isComplete){
+	transporter.sendMail(mailOpt, function(error, info){
+		if(error){
+			return console.log(error);
+		}
+		console.log('Message sent: ' + info.response );
+	});
+}else{
+	
+	console.log('Error on MailOptions');
+	
+}
