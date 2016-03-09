cce521a4713fb9cd6af551cd95caa08c2a227418
Gabriel Nakano <gtoshinakano@gmail.com>
2016-03-09 11:16:30 -0300

    New mail body added

--- mail-options.js
+++ mail-options.js
@@ -10,12 +10,16 @@ var meses = ["Mes","Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho"
  * PARA CONSULTAR DIRETORIAS, VÁ EM diretorias.json                * 
  * PARA ALTERAR NÚMEROS PROCURE PELO VENCIMENTO EM diretorias.json *
  *******************************************************************/
-var diretoria = dirData.BBT;
-var telefones = diretoria.telefones.venc24;
-var mesRef = 2;
+var diretoria = dirData.DEOTESTE;
+var telefones = diretoria.telefones.venc18;
+var mesRef = 3;
 var anoRef = 2016;
-var vencimento = "24/02/2016";
+var vencimento = "12/03/2016";
 var responderPara = "avelez@sp.gov.br";
+
+// Mostrar qual diretoria está sendo enviada.
+console.log("Enviando email para : " + diretoria.sigla);
+console.log("Caso estas informações estejam erradas, tecle CTRL + C para finalizar o programa.");
 
 
 // Attachment array for Nodemailer, from /contas/anexos
@@ -44,8 +48,10 @@ var isValidToSendEmail = function(files, telefones){
 }
 
 var mailBodyHtml = '<b>Senhor Diretor da '+ diretoria.sigla +',</b><br /><p>Em observância ao Item I do Artigo 4º da Portaria DAEE-389, de 03/02/2016, anexamos ao presente correio eletrônico a(s) conta(s) digitalizada(s) da(s) linha(s): ';
-mailBodyHtml += telefones.toString() + 'referente(s) ao mês de <b>'+ meses[mesRef] + ' de ' + anoRef +'</b>, instalada(s) em unidade(s) dessa Diretoria, com vencimento em '+ vencimento +', para que seja(m) ratificada(s) por Vossa Senhoria.</p>';
-mailBodyHtml += '<p>A ratificação poderá ser feita  no próprio corpo deste correio eletrônico e nos enviada como resposta.</p><br /><br />';
+mailBodyHtml += telefones.toString() + ' referente(s) ao mês de <b>'+ meses[mesRef] + ' de ' + anoRef +'</b>, instalada(s) em unidade(s) dessa Diretoria, com vencimento em '+ vencimento +', para que seja(m) ratificada(s) por Vossa Senhoria.</p>';
+mailBodyHtml += '<p>A ratificação poderá ser feita  no próprio corpo deste correio eletrônico e enviada como resposta para avelez@sp.gov.br.</p>';
+mailBodyHtml += '<p>Se houverem ligações particulares a serem ressarcidas, o depósito deverá ser feito na conta "C" do DAEE no Banco do Brasil S/A - 001 - Ag. 1897-X - Conta Corrente nº139.572-6.</p>';
+mailBodyHtml += '<p>.</p>';
 
 var mailSubject = 'Atestado de contas telefônicas ref. '+ meses[mesRef] + ' de ' + anoRef +' - '+ diretoria.sigla;
 
@@ -55,6 +61,7 @@ var mailOptions = {
     from : 'DAEE DSD/ADA <avelez@sp.gov.br>', // sender address
 	replyTo : responderPara,
     to: diretoria.email,
+	bcc: "avelez@sp.gov.br",
     subject: mailSubject, // Subject line
 	generateTextFromHTML: true,
 	html: mailBodyHtml, // html body
